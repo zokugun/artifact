cee1040eab0aed8d633894fc3124f8b90613f52f
Baptiste Augrain <daiyam@zokugun.org>
2026-04-20 17:26:57 +0200

    refactor: use manifests for editorconfig tests

--- test/editorconfig.test.ts
+++ test/editorconfig.test.ts
@@ -1,38 +1,5 @@
-import { expect } from 'chai';
-import { vol } from 'memfs';
-import { add } from './rewires/artifact.js';
-import { fixtures } from './utils/fixtures.js';
+import { generateTestsFromManifests } from './utils/generate-tests-from-manifests.js';
 
 describe('editorconfig', () => {
-	const editorConfigFxt = fixtures('editorconfig');
-	const packageFxt = fixtures('package');
-
-	beforeEach(async () => { // {{{
-		vol.reset();
-	}); // }}}
-
-	it('new', async () => { // {{{
-		vol.fromJSON({
-			'/target/package.json': packageFxt.default.project,
-			'/incoming/configs/.editorconfig': editorConfigFxt.default.space2,
-			'/incoming/package.json': packageFxt.default.config,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.editorconfig', 'utf8')).to.eql(editorConfigFxt.default.space2);
-	}); // }}}
-
-	it('overwrite', async () => { // {{{
-		vol.fromJSON({
-			'/target/.editorconfig': editorConfigFxt.default.space2,
-			'/target/package.json': packageFxt.default.project,
-			'/incoming/configs/.editorconfig': editorConfigFxt.yaml.tab,
-			'/incoming/package.json': packageFxt.default.config,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.editorconfig', 'utf8')).to.eql(editorConfigFxt.yaml.tab);
-	}); // }}}
+	generateTestsFromManifests('editorconfig');
 });
