b52fe41e6f84eeb270208f97edb183253b9c45c8
Baptiste Augrain <daiyam@zokugun.org>
2026-04-20 16:52:41 +0200

    refactor: use manifests for gitignore tests

--- test/gitignore.test.ts
+++ test/gitignore.test.ts
@@ -1,38 +1,5 @@
-import { expect } from 'chai';
-import { vol } from 'memfs';
-import { add } from './rewires/artifact.js';
-import { fixtures } from './utils/fixtures.js';
+import { generateTestsFromManifests } from './utils/generate-tests-from-manifests.js';
 
 describe('gitignore', () => {
-	const ignoreFxt = fixtures('ignore');
-	const packageFxt = fixtures('package');
-
-	beforeEach(async () => { // {{{
-		vol.reset();
-	}); // }}}
-
-	it('new', async () => { // {{{
-		vol.fromJSON({
-			'/target/package.json': packageFxt.default.project,
-			'/incoming/configs/gitignore': ignoreFxt.default.merged,
-			'/incoming/package.json': packageFxt.default.config,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.gitignore', 'utf8')).to.eql(ignoreFxt.default.merged);
-	}); // }}}
-
-	it('merge', async () => { // {{{
-		vol.fromJSON({
-			'/target/.gitignore': ignoreFxt.merge.target,
-			'/target/package.json': packageFxt.default.project,
-			'/incoming/configs/gitignore': ignoreFxt.merge.incoming,
-			'/incoming/package.json': packageFxt.default.config,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.gitignore', 'utf8')).to.eql(ignoreFxt.merge.merged);
-	}); // }}}
+	generateTestsFromManifests('gitignore');
 });
