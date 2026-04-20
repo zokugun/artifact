29bb09b4efdd4fb0fb7f773825aff46dc9eecebb
Baptiste Augrain <daiyam@zokugun.org>
2026-04-20 17:09:31 +0200

    refactor: use manifests for version tests

--- test/utils/generate-tests-from-manifests.ts
+++ test/utils/generate-tests-from-manifests.ts
@@ -1,12 +1,16 @@
 import path from 'node:path';
 import process from 'node:process';
 import { isArray, isRecord, isString } from '@zokugun/is-it-type';
-import { expect } from 'chai';
+import { expect, use } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import { vol } from 'memfs';
 import YAML from 'yaml';
 import { add, remove, update } from '../rewires/artifact.js';
+
 // eslint-disable-next-line import/order
 import fse from '@zokugun/fs-extra-plus/sync';
+
+use(chaiAsPromised);
 
 const DEBUG = process.env.DEBUG === '1' || process.env.DEBUG === 'true' || process.env.DEBUG === 'on';
 
--- test/version.test.ts
+++ test/version.test.ts
@@ -1,45 +1,5 @@
-import { expect, use } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import { vol } from 'memfs';
-import { add, update } from './rewires/artifact.js';
-import { fixtures } from './utils/fixtures.js';
-
-use(chaiAsPromised);
+import { generateTestsFromManifests } from './utils/generate-tests-from-manifests.js';
 
 describe('version', () => {
-	const versionFxt = fixtures('version');
-	const packageFxt = fixtures('package');
-
-	beforeEach(async () => { // {{{
-		vol.reset();
-	}); // }}}
-
-	it('v1.update.strict', async () => { // {{{
-		vol.fromJSON({
-			'/target/.artifactrc': versionFxt.v1UpdateStrict.targetArtifactrc,
-			'/target/package.json': packageFxt.default.project,
-			'/target/src/index.ts': versionFxt.v1UpdateStrict.targetSrc,
-			'/incoming/.artifactrc': versionFxt.v1UpdateStrict.incomingArtifactrc,
-			'/incoming/package.json': packageFxt.default.config,
-			'/incoming/configs/src/index.ts': versionFxt.v1UpdateStrict.incomingSrc,
-		}, '/');
-
-		await update();
-
-		expect(vol.readFileSync('/target/.artifactrc', 'utf8')).to.eql(versionFxt.v1UpdateStrict.mergedArtifactrc);
-		expect(vol.readFileSync('/target/src/index.ts', 'utf8')).to.eql(versionFxt.v1UpdateStrict.targetSrc);
-	}); // }}}
-
-	it('v999.install', async () => { // {{{
-		vol.fromJSON({
-			'/target/package.json': packageFxt.default.project,
-			'/incoming/package.json': packageFxt.default.config,
-			'/incoming/.artifactrc.yml': versionFxt.v999.incoming,
-		}, '/');
-
-		// eslint-disable-next-line @typescript-eslint/no-floating-promises
-		expect(add(['awesome-config'])).to.be.rejectedWith('Error: Don\'t support newer version (v999) in the package\'s ".artifactrc.yml"');
-
-		expect(vol.existsSync('/target/.artifactrc')).to.be.false;
-	}); // }}}
+	generateTestsFromManifests('version');
 });
