d732f7d2ff5f7b31bcc6675815efc001da1286b4
Baptiste Augrain <daiyam@zokugun.org>
2026-04-20 16:50:06 +0200

    refactor: use manifests for fixpack tests

--- test/fixpack.test.ts
+++ test/fixpack.test.ts
@@ -1,49 +1,5 @@
-import { expect } from 'chai';
-import { vol } from 'memfs';
-import { add } from './rewires/artifact.js';
-import { fixtures } from './utils/fixtures.js';
+import { generateTestsFromManifests } from './utils/generate-tests-from-manifests.js';
 
 describe('fixpack', () => {
-	const fixpackFxt = fixtures('fixpack');
-	const packageFxt = fixtures('package');
-
-	beforeEach(async () => { // {{{
-		vol.reset();
-	}); // }}}
-
-	it('rc.json', async () => { // {{{
-		vol.fromJSON({
-			'/target/.fixpackrc': fixpackFxt.json.target,
-			'/incoming/package.json': packageFxt.default.config,
-			'/incoming/configs/.fixpackrc': fixpackFxt.json.incoming,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.fixpackrc', 'utf8')).to.eql(fixpackFxt.json.merged);
-	}); // }}}
-
-	it('rc.yaml', async () => { // {{{
-		vol.fromJSON({
-			'/target/.fixpackrc': fixpackFxt.yaml.target,
-			'/incoming/package.json': packageFxt.default.config,
-			'/incoming/configs/.fixpackrc': fixpackFxt.yaml.incoming,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.fixpackrc', 'utf8')).to.eql(fixpackFxt.yaml.merged);
-	}); // }}}
-
-	it('sort', async () => { // {{{
-		vol.fromJSON({
-			'/target/.fixpackrc': fixpackFxt.sort.target,
-			'/incoming/package.json': packageFxt.default.config,
-			'/incoming/configs/.fixpackrc': fixpackFxt.sort.incoming,
-		}, '/');
-
-		await add(['awesome-config']);
-
-		expect(vol.readFileSync('/target/.fixpackrc', 'utf8')).to.eql(fixpackFxt.sort.merged);
-	}); // }}}
+	generateTestsFromManifests('fixpack');
 });
